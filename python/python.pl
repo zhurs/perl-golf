863ccdfb5458a5c9b5de0005822e5cb581c32c87
Your Name <zhur@LENOVO-28BD2090.(none)>
2009-05-20 01:00:59 +0400

    Optimized by flags

--- python/python.pl
+++ python/python.pl
@@ -1,15 +1,16 @@
-#!perl
+#!perl -lnaF
 sub t{$.*$w-$w+$-[0]}
-while(<>){
-@m=split/\s*/;
-$w=push(@f,@m)/$. if!/[lhjk]/;
-$p[1]=$h=t if/w/;
-$p[0]=t if/0/;
-}
-for(@m){
+if(/[lhjk]/){
+for(@F){
 $f[$h]='0';
 $h+=/l/?1:/h/?-1:/j/?-$w:$w;
-$f[shift @p]='x' if $f[$h]==0;
-$f[$p[@p]=$h]='w';
+$f[shift@p]='x'if$f[$h]==0;
+$f[$p[@p]=$h]='w'
 }
-while(@f){print splice(@f,0,$w),"\n"}
+while(@f){print splice(@f,0,$w)}
+}else{
+$w=push(@f,@F)/$.;
+$p[1]=$h=t if/w/;
+$p[0]=t if/0/
+}
+
